41d8e46ac0ec99608cfdcbf9d4dd92752e76451a
RikRhino <kwiki@scarlet.be>
2022-01-23 08:53:55 +0100

    Updated static_root

--- renderworks/settings.py
+++ renderworks/settings.py
@@ -119,9 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 
