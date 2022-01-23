0ffca052e4374478573a950da2f5ea009a278196
RikRhino <kwiki@scarlet.be>
2022-01-23 09:27:35 +0100

    env vars

--- renderworks/settings.py
+++ renderworks/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8ln*27-r=^bzzvobhq(+ec3r1d84ta(nymyip(0iy-wl5b8m8g'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+#django-insecure-8ln*27-r=^bzzvobhq(+ec3r1d84ta(nymyip(0iy-wl5b8m8g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kwikenkwak.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
